c8f09b18a9c7d47dfafc7d60537801fdb1d07f13
Nareg Boynukalın <naregboynukalin@gmail.com>
2019-01-31 03:12:32 +0300

    Rename Date.cpp to CH3/Date.cpp

--- CH3/Date.cpp
+++ CH3/Date.cpp
@@ -3,15 +3,15 @@
 
 // Self-review exercises from Chapter 3 "How to program C++ 9th edition"
 
-// Create a class called Date that includes three pieces of information as data members —
+// Create a class called Date that includes three pieces of information as data members â€”
 // a month (type int), a day (type int) and a year (type int). Your class should have 
 // a constructor with three parameters that uses the parameters to initialize the three
 // data members. For the purpose of this exercise, assume that the values provided for
-// the year and day are correct, but ensure that the month value is in therange 1–12;
-// if it isn’t, set the month to 1. Provide a set and a get function for each data member.
+// the year and day are correct, but ensure that the month value is in therange 1â€“12;
+// if it isnâ€™t, set the month to 1. Provide a set and a get function for each data member.
 // Provide a member function displayDate that displays the month, day and year
 // separated by forward slashes (/). Write a test program that demonstrates
-// class Date’s capabilities.
+// class Dateâ€™s capabilities.
 
 
 #include<iostream>
